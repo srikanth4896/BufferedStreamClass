c12ac09f85c84cab24571ac3a061209a6d4ce2a1
Srikanth <srikanth.chilamkuri@zensar.com>
2021-01-27 18:51:43 +0530

    Add project files.

---
+++ BufferedStreamClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BufferedStreamClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          FileStream fileStream =File.Create(@"F:\text21.txt");
+            BufferedStream bs = new BufferedStream(fileStream, 100);
+            for (int i= 0; i <100; i++)
+            {
+                string s = "This is line " + i + "\n";
+                byte[] bytes = Encoding.UTF8.GetBytes(s);
+                bs.Write(bytes, 0, bytes.Length);
+            }
+            bs.Flush();
+        }
+    }
+}
